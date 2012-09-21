330562d04538b4a94bb29446d72196dfd5633c1a
stefanjeffers <stefanjeffers@gmail.com>
2012-09-21 02:36:19 -0700

    Initial commit for char app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharApp::Application.config.secret_token = 'bb4999f505000f1dd94928f0204f16cb64cf482f5849b2d6d286f9575400239ca3a0582927a68341509684eb53def32f72d620d646112184dbafc22506fd8609'
